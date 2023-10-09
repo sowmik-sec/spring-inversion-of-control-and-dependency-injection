8a8480e1efdb7ce95de8d88c2d1dcc36327951b3
sowmik-sec <sowmik.sec@gmail.com>
2023-10-09 13:04:02 +0600

    qualifier annotation

--- src/main/java/com/sowmik/springcoredemo/rest/DemoController.java
+++ src/main/java/com/sowmik/springcoredemo/rest/DemoController.java
@@ -2,6 +2,7 @@ package com.sowmik.springcoredemo.rest;
 
 import com.sowmik.springcoredemo.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,15 +11,15 @@ public class DemoController {
     private Coach myCoach;
 
     // constructor injection
-//    @Autowired
-//    public DemoController(Coach theCoach) {
-//        myCoach = theCoach;
-//    }
-    // setter injection
     @Autowired
-    public void setCoach(Coach theCoach) { // function name can be anything
+    public DemoController(@Qualifier("cricketCoach") Coach theCoach) {
         myCoach = theCoach;
     }
+    // setter injection
+//    @Autowired
+//    public void setCoach(Coach theCoach) { // function name can be anything
+//        myCoach = theCoach;
+//    }
     @GetMapping("/dailyworkout")
     public String getDailyWorkout() {
         return myCoach.getDailyWorkout();
